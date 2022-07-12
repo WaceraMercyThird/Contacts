ec5e09ddc27ba611300a04bd4a26b43e6a152285
Mercy Wacera <waceraaamercy@gmail.com>
2022-07-12 20:28:16 +0300

    Display image with picasso

--- app/src/main/java/dev/waceramercythird/mycontacts/ContactsRvAdapter.kt
+++ app/src/main/java/dev/waceramercythird/mycontacts/ContactsRvAdapter.kt
@@ -1,10 +1,13 @@
 package dev.waceramercythird.mycontacts
 
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.squareup.picasso.Picasso
 import dev.waceramercythird.mycontacts.databinding.ContactListItemsBinding
@@ -18,6 +21,7 @@ class ContactsRvAdapter(var contactList: List<Contact>) :
     }
 
     override fun onBindViewHolder(holder: ContactsViewHolder, position: Int) {
+        var context = holder.itemView.context
         var currentContact = contactList.get(position)
         with(holder.binding) {
             tvName.text = currentContact.name
@@ -31,8 +35,24 @@ class ContactsRvAdapter(var contactList: List<Contact>) :
                 .placeholder(R.drawable.ic_baseline_person_24)
                 .error(R.drawable.ic_baseline_error_24)
                 .into(holder.binding.imgContact)
+//            cvContact.setOnClickListener{
+//                var context = holder.itemView.context
+//                var intent = Intent(context, ViewContactActivity::class.java)
+//                context.startActivity(intent)
+//                holder.itemView.context.startActivities()
+            }
+        holder.binding.cvContact.setOnClickListener{
+            var intent = Intent(context, ViewContactActivity::class.java)
+            intent.putExtra("NAME", currentContact.name)
+            intent.putExtra("PHONE_NUMBER",currentContact.number)
+            context.startActivity(intent)
 
 
+        }
+        holder.binding.imgContact.setOnClickListener{
+            Toast
+                .makeText(context, "You have clicked on my face", Toast.LENGTH_LONG)
+                .show()
         }
 
     }
---
+++ app/src/main/java/dev/waceramercythird/mycontacts/ViewContactActivity.kt
@@ -0,0 +1,21 @@
+package dev.waceramercythird.mycontacts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class ViewContactActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_contact)
+        getExtras()
+    }
+    fun getExtras(){
+        val extras = intent.extras
+        var name= extras?.getString("NAME", "")
+        var phone = extras?.getString("PHONE_NAME","")
+
+
+        Toast.makeText(this,name,Toast.LENGTH_LONG).show()
+    }
+}
