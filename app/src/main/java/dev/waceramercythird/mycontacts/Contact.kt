dbfedf6c37da746965c3c1323f98c123e14b83af
Mercy Wacera <waceraaamercy@gmail.com>
2022-07-13 22:52:36 +0300

    View Activity

--- app/src/main/java/dev/waceramercythird/mycontacts/Contact.kt
+++ app/src/main/java/dev/waceramercythird/mycontacts/Contact.kt
@@ -4,8 +4,6 @@ data class Contact(var name: String,
                    var number: String,
                    var email: String,
                    var address: String,
-                   var image: String,
-                   var whats: String,
-                   var message: String
+                   var image: String
                    )
 //var mercy =
